4418b2328a2dad5897a112c9423c7bca4e90b8cf
Kseniya Dergunova <kdergunova@vocalize.ru>
2020-05-03 15:29:13 +0300

    Компонент. Модуль. [#61901]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { StatisticComponent } from './task-list/statistic/statistic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TaskListComponent,
+    StatisticComponent
   ],
   imports: [
     BrowserModule
